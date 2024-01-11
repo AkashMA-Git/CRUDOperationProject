bec13cf1a2588129797a750600e05bfd2a27934b
AKASH M A <abiakash1993@gmail.com>
2024-01-11 12:18:05 +0530

    Day2Task without additional things

--- CRUDOperationProject/Controllers/EmployeeController.cs
+++ CRUDOperationProject/Controllers/EmployeeController.cs
@@ -19,13 +19,84 @@ namespace CRUDOperationProject.Controllers
         public IActionResult Create() { 
             return View();
         }
-        public IActionResult Update()
-        {
-            return View();
+       
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {   
+            if(ModelState.IsValid)
+            {
+                _context.Employees.Add(obj);
+                _context.SaveChanges();
+                TempData["Success"] = "New Employee Added Successfully";
+                return RedirectToAction("Index");
+            }
+               return View();
+           
         }
-        public IActionResult Delete()
+       
+
+        
+        public IActionResult Update(int? id)
         {
+            if (id == null || id == 0)
+            {   
+                return NotFound();
+            }
+            Employee? employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            if(employee == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(employee);
+
+        }
+        [HttpPost]
+        public IActionResult Update(Employee obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(obj);
+                _context.SaveChanges();
+                TempData["Success"] = " Employee details updated Successfully";
+
+                return RedirectToAction("Index");
+            }
             return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Employee? employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(employee);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {   
+            Employee? obj = _context.Employees.FirstOrDefault(x =>x.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(obj);
+            _context.SaveChanges();
+            TempData["Success"] = "  Employee details deleted Successfully";
+
+            return RedirectToAction("Index");
+            
+            
         }
 
 
